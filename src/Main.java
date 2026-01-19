0e91c20cc75a33f4fcfcf21d2514553033219d2a
Roberto Fusella <r.fusella@begear.it>
2026-01-19 14:58:43 +0100

    rimosso il ciclo for

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,7 @@ void main() {
     } catch (InputMismatchException e) {
         System.out.println("inserisci un valore valido");
     }
+    //prova git
     System.out.println("nuova funzione");
-    for(int i=0; i == 0;){
-        System.out.println("nuova funzione");
-    }
+
 }
