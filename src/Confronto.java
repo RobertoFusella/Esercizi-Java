deb34e2908f6a010f66888748592b6c45625412d
Roberto Fusella <r.fusella@begear.it>
2026-01-20 11:14:08 +0100

    aggiunta classe con metodi

--- src/Confronto.java
+++ src/Confronto.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Confronto {
+   private int primoValore;
+   private int secondoValore;
+
+   public void controllo(int primoValore,int secondoValore){
+        if(primoValore > secondoValore)
+            System.out.println(primoValore + " è maggiore di " + secondoValore);
+        else if (secondoValore > primoValore)
+            System.out.println(secondoValore + " è maggiore di " + primoValore);
+        else if(primoValore == secondoValore || secondoValore == primoValore)
+            System.out.println("i numeri sono uguali");
+   }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,12 @@ void main() {
     int valore1 = scanner.nextInt();
     System.out.println("inserisci secondo valore");
     int valore2 = scanner.nextInt();
-
-
-        if (valore1 > valore2)
-            System.out.println(valore1 + " è maggiore di " + valore2);
-        else if (valore2 > valore1)
-            System.out.println(valore2 + " è maggiore di " + valore1);
-        else if (valore1 == valore2)
-            System.out.println("i numeri sono uguali");
+    Confronto confronto = new Confronto();
+    confronto.controllo(valore1,valore2);
         //un errore che ho incontrato su questo esercizio era l'input
         //se inserivo qualcosa di diverso da un intero (es. un char)
         //mi dava come errore: InputMismatchException
     } catch (InputMismatchException e) {
-        System.out.println("inserisci un valore valido");
+        System.out.println("valore non valido");
     }
-    //prova git
-    System.out.println("nuova funzione");
-
 }
