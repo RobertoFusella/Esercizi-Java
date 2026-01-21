2f058a864f0ba35e1753caa185263e72fd26dfdc
Roberto Fusella <r.fusella@begear.it>
2026-01-21 12:29:24 +0100

    rivisitata la logica di controllo, aggiunto ciclo while

--- src/settimana1/Esercizio1.java
+++ src/settimana1/Esercizio1.java
@@ -14,29 +14,34 @@ public class Esercizio1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int valore1;
+        int valore2;
+        boolean inputNonValido=false;
         /*
          TODO riscrivi in modo che se venisse inserito un carattere non numerico,
          viene richiesto il valore invece che terminare con un errore
          */
-        try {
-            System.out.println("inserisci primo valore");
-            int valore1 = scanner.nextInt();
-            System.out.println("inserisci secondo valore");
-            int valore2 = scanner.nextInt();
-            Confronto confronto = new Confronto();
-            confronto.controllo(valore1, valore2);
-            //un errore che ho incontrato su questo esercizio era l'input
-            //se inserivo qualcosa di diverso da un intero (es. un char)
-            //mi dava come errore: InputMismatchException
-        } catch (InputMismatchException e) {
+        while (!inputNonValido)
+            try {
+                System.out.println("inserisci primo valore");
+                valore1 = scanner.nextInt();
+                System.out.println("inserisci secondo valore");
+                valore2 = scanner.nextInt();
 
-            /*
-             suggerimento in caso di errore bisogna sempre stampare lo stack trace, in questo caso
-             e.printStackTrace();
-             */
+                Confronto confronto = new Confronto();
+                confronto.controllo(valore1, valore2);
+                inputNonValido=true;
 
-            System.out.println("valore non valido");
+            } catch (InputMismatchException e) {
+                System.out.println("errore riprova");
+                scanner.nextLine();
+            }
         }
+                //un errore che ho incontrato su questo esercizio era l'input
+                //se inserivo qualcosa di diverso da un intero (es. un char)
+                //mi dava come errore: InputMismatchException
+
+                //un altro errore che ho incontrato è stato che se non pulivo
+                //lo scanner con .nextLine() l'input sbagliato rimaneva nel buffer
+                //e partiva un loop infinito
     }
-}
