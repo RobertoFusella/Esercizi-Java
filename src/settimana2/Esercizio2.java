390dba3e955167cba69d890e981bb8dd11abce18
Roberto Fusella <r.fusella@begear.it>
2026-01-22 12:19:08 +0100

    primo commit

--- src/settimana2/Esercizio2.java
+++ src/settimana2/Esercizio2.java
@@ -1,7 +1,7 @@
 package settimana2;
-
+import settimana1.Confronto;
+import java.util.Scanner;
 public class Esercizio2 {
-
     /*
     TODO
     Usando il codice di settimana1.Esercizio1
@@ -12,4 +12,30 @@ public class Esercizio2 {
 
     Spiega come hai migliorato la leggibilità del codice
      */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int primoValore;
+        int secondoValore;
+        boolean inputNonValido = false;
+        Confronto confronto = new Confronto();
+
+        do {
+            System.out.println("Inserisci primo valore");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Errore, riprova:");
+                scanner.next();
+            }
+            primoValore = scanner.nextInt();
+            System.out.println("Inserisci secondo valore");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Errore, riprova:");
+                scanner.next();
+            }
+            secondoValore = scanner.nextInt();
+            inputNonValido = true;
+        } while (!inputNonValido);
+
+
+        confronto.controllo(primoValore, secondoValore);
+    }
 }
