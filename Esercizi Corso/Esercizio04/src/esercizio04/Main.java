87d9111ba8f10ef060ef66aacfe192e8c61142f4
Roberto Fusella <r.fusella@begear.it>
2026-02-03 09:52:35 +0100

    Ripristino di tutti gli esercizi e sistemazione cartelle

---
+++ Esercizi Corso/Esercizio04/src/esercizio04/Main.java
@@ -0,0 +1,9 @@
+void main() {
+    int studenti = 300;
+    int stanze = 0;
+
+    if (stanze != 0 && studenti / stanze > 30)
+        System.out.println("affolato");
+    else
+        System.out.println("stanze non disponibili");
+}
