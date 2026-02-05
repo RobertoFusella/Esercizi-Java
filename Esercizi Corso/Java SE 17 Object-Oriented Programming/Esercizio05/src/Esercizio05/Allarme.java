38b46145161b61a951e8dee013457dee638df480
Roberto Fusella <r.fusella@begear.it>
2026-02-05 13:49:42 +0100

    aggiunto esercizio05

--- Esercizi Corso/Java SE 17 Object-Oriented Programming/Esercizio05/src/Esercizio05/Allarme.java
+++ Esercizi Corso/Java SE 17 Object-Oriented Programming/Esercizio05/src/Esercizio05/Allarme.java
@@ -7,9 +7,9 @@ public class Allarme {
     //rendendo messaggio final stiamo dicendo che non può
     //più essere cambiato una volta assegnato e il suo valore deve essere
     //assegnato sennò il compilatore darà errore
-    final String messaggio;
+    private final String messaggio;
     //LocalDateTime è una classe di java che specifica un momento preciso nel tempo
-    LocalDateTime dormiFino;
+    private LocalDateTime dormiFino;
 
     public Allarme(String message) {
         this.messaggio = message;
@@ -17,35 +17,61 @@ public class Allarme {
         //la sveglia non è attiva
         nonDormendo();
     }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public String getMessaggio() {
+        return messaggio;
+    }
+
+    public LocalDateTime getDormiFino() {
+        return dormiFino;
+    }
+
+    public void setDormiFino(LocalDateTime dormiFino) {
+        this.dormiFino = dormiFino;
+    }
+
     //metodo che accende l'allarme
     public void accendiAllarme() {
         active = true;
+        nonDormendo();
     }
+
     //metodo che spegne l'allarme
-    void spegniAllarme() {
+    public void spegniAllarme() {
         active = false;
+        nonDormendo();
     }
+
     //questo metodo serve a dire aggiungi 5min rispetto al tempo attuale
-    void dormi(){
-        dormiFino = LocalDateTime.now().plusMinutes(5);
+    public void dormi() {
+        if (active) {
+            dormiFino = LocalDateTime.now().plusMinutes(5);
+        }
     }
+
     //controlla se la variabile dormiFino è nel futuro
-    boolean staDormendo(){
+    public boolean staDormendo() {
         return (dormiFino.isAfter(LocalDateTime.now()));
     }
+
     //controlla se dormiFino è nel passato di 1sec
-    void nonDormendo(){
+    private void nonDormendo() {
         dormiFino = LocalDateTime.now().minusSeconds(1);
     }
 
     //metodo overload di quello originale stampa il messaggio
     //tutto in minuscolo invece di farlo in maiuscolo
-    String riceviNotifica() {
+    public String riceviNotifica() {
         return riceviNotifica(false);
     }
+
     //prende un valore booleano se true rende tutto il messaggio maiuscolo
     //se false rimane come è
-    String riceviNotifica(boolean maiuscola) {
+    public String riceviNotifica(boolean maiuscola) {
         //controlla se la sveglia è attiva e staDormendo è true
         if (active && !staDormendo()) {
             if (maiuscola) {
@@ -57,6 +83,7 @@ public class Allarme {
             return "";
         }
     }
+
     //stampa il messaggio e decide se farlo in maiuscolo o minuscolo
     public void inviaNotifica() {
         System.out.println(riceviNotifica(true));
--- Esercizi Corso/Java SE 17 Object-Oriented Programming/Esercizio05/src/Esercizio05/Esercizio05.java
+++ Esercizi Corso/Java SE 17 Object-Oriented Programming/Esercizio05/src/Esercizio05/Esercizio05.java
@@ -1,9 +1,11 @@
 package Esercizio05;
 
-public class Main {
-    static void main(String[] args) {
+public class Esercizio05 {
+    static void main(String[] args) throws InterruptedException {
         Allarme allarme = new Allarme("temperatura troppo alta");
         allarme.accendiAllarme();
+        allarme.staDormendo();
+        Thread.sleep(5000);
         allarme.inviaNotifica();
     }
 }
