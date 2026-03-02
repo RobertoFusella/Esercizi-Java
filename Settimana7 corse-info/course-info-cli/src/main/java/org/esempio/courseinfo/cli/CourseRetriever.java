c7634f380149c1d54b55c2cf17ee536feb62e567
Roberto Fusella <r.fusella@begear.it>
2026-03-02 14:53:44 +0100

    corretto alcune descrizioni e aggiunto esercizi settimana7

--- Settimana7 corse-info/course-info-cli/src/main/java/org/esempio/courseinfo/cli/CourseRetriever.java
+++ Settimana7 corse-info/course-info-cli/src/main/java/org/esempio/courseinfo/cli/CourseRetriever.java
@@ -5,6 +5,8 @@ import org.esempio.courseinfo.cli.service.PluralsightCourse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class CourseRetriever {
     private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
     public static void main(String... args) {
@@ -15,6 +17,7 @@ public class CourseRetriever {
         }
         try {
             retrieverCourses(args[0]);
+            //utilizzo del record
 //            PluralsightCourse course =
 //                    new PluralsightCourse("id","title","00:20:14","https://url",false);
 //            LOG.info("course: {}", course);
@@ -27,7 +30,10 @@ public class CourseRetriever {
         LOG.info("Retrieving courses for author '{}'", authorId);
         CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
 
-        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
-        LOG.info("Retrieved the following courses: {}", coursesToStore);
+        List<PluralsightCourse> coursesToStore = courseRetrievalService.getCoursesFor(authorId)
+                .stream()
+                .filter(course -> !course.isRetired())
+                .toList();
+        LOG.info("Retrieved the following {} courses: {}",coursesToStore.size() ,coursesToStore);
     }
 }
--- Settimana7 corse-info/src/main/java/org/esempio/courseinfo/cli/service/PluralsightCourse.java
+++
@@ -1,5 +0,0 @@
-package org.esempio.courseinfo.cli.service;
-
-public record PluralsightCourse(String id, String title, String duration, String contentUrl, boolean isRetired) {
-
-}
